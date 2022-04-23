39ebd1977474831de625ff96cad8f94bfc269736
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-04-23 21:26:29 +0800

    fix

--- reddit_bot.py
+++ reddit_bot.py
@@ -43,8 +43,8 @@ def run_bot(r, comments_replied_to):
 
 
 def get_saved_comments():
-    comments_replied_to = []
-    if os.path.isfile("comments_replied_to.txt"):
+    # comments_replied_to = []
+    if not os.path.isfile("comments_replied_to.txt"):
         comments_replied_to = []
     else:
         with open("comments_replied_to.txt", "r") as f:
@@ -58,7 +58,7 @@ def get_saved_comments():
 r = bot_login()
 comments_replied_to = []
 temp = get_saved_comments()
-for i in comments_replied_to:
+for i in temp:
     comments_replied_to.append(i)
 
 while True:
---
+++ test.py
@@ -0,0 +1,19 @@
+import os
+
+
+def get_saved_comments():
+    # comments_replied_to = []
+    if not os.path.isfile("comments_replied_to.txt"):
+        comments_replied_to = []
+    else:
+        with open("comments_replied_to.txt", "r") as f:
+            comments_replied_to = f.read()
+            comments_replied_to = comments_replied_to.split("\n")
+            comments_replied_to = filter(None, comments_replied_to)
+
+    return comments_replied_to
+
+
+temp = get_saved_comments()
+for i in temp:
+    print(i)
