ad5aefe223ab2cbc885e852769d08f733f86cb6b
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2022-04-23 21:05:43 +0800

    update

---
+++ config.py
@@ -0,0 +1,10 @@
+username = "RedditUsername"
+password = "passward"
+client_id = "idGoesHere"
+client_secret = "secretGoesHere"
+
+trigger = [
+    {'trigger': "Bear", 'reply': 'Mr Xi is the greate Sun in China!'},
+    {'trigger': "Universe", 'reply': 'Universe is very big'},
+    {'trigger': "Bad bot", 'reply': 'I feel so sorry, because I am just a bot'}
+]
